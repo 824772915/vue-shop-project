1e7ac549b40b479a85f49bee5b666b7181a9c3ab
824772915 <824772915@qq.com>
2021-08-20 16:08:06 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       <p>
         If Element is successfully added to this project, you'll see an
         <code v-text="'<el-button>'"></code>
-        below
+        belo
       </p>
       <el-button>el-button</el-button>
     </div>
