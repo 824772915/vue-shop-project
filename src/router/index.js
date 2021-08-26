e3720016082fd9b09470b1c77ba15485bd2af85a
824772915 <824772915@qq.com>
2021-08-26 18:00:55 +0800

    完成部分分类功能

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Users from "../components/Users/Users.vue";
 import Rights from "../components/Power/Right.vue";
 import Roles from "../components/Power/Roles.vue";
 import Cate from "../components/Goods/Cate.vue";
+import Params from '../components/Goods/Params.vue';
 
 Vue.use(VueRouter)
 
@@ -39,7 +40,10 @@ const routes = [
       },
       {path:'/categories',
       component:Cate
-    }
+      },
+      {path:'/params',
+      component:Params
+      },
     ]
   }
 ]
